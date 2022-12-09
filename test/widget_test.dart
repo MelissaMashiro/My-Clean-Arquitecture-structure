df09bda3394ce449aafcde94998cb93fec6d095e
Melissa Alvarez <softwareprojectsmeli@gmail.com>
2022-12-09 08:38:10 -0500

    wigget test file created

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,57 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility in the flutter_test package. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+import 'package:clean_arquitecture_project/src/domain/entities/soccer_match.dart';
+import 'package:clean_arquitecture_project/src/domain/repositories/soccerboard_repository.dart';
+import 'package:clean_arquitecture_project/src/domain/usecases/soccerboard/live_matchs_list_use_case.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+class MockPostRepository extends Mock implements SoccerboardRepository {}
 
+void main() {
+  late LiveMatchsListUseCase liveMatchsListUseCase;
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(const MyApp());
+  late MockPostRepository repository;
 
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
+  setUp(() {
+    repository = MockPostRepository();
 
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
+    liveMatchsListUseCase =
+        LiveMatchsListUseCase(soccerboardRepository: repository);
+  });
 
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+  //CREANDO EL MOCK DE LA RESPUESTA:
+
+  final soccerMatchsFromService = <SoccerMatch>[
+    // SoccerMatch(title: 'Test 1', content: 'Test 1 content'),
+  ];
+
+  test('Should get all live matches from server', () async {
+    //MOCKEANDO LA RESPUESTA: Se debe hacer directamente al metodo del repo q se usará, no al usecase
+    when(
+      () => repository.getLiveMatched(
+        year: '2021',
+      ),
+    ).thenAnswer(
+      (_) async => Right(soccerMatchsFromService),
+    );
+
+//implementando endpoint
+    final result = await liveMatchsListUseCase(arguments: {
+      'year': '2021',
+    });
+
+//verificando que hayamos tenido una respuesta exitosa con el endpoint
+    expect(
+      result,
+      Right(soccerMatchsFromService),
+    );
+
+//Verify that a method on a mock object was called with the given arguments.
+
+    verify(
+      () => repository.getLiveMatched(year: '2021'),
+    );
+
+    verifyNoMoreInteractions(repository);
+  });
+}
